37155012c5fb4719aed5087d6db1c4da4abcf90e
Mayank Kumar Gautam <mayank.guts30@gmail.com>
2025-03-28 14:42:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function btn () {
+	
+
+let optionss = document.getElementById("colorSelect");
+	let selectedOptionIndex = optionss.selectedIndex;
+
+	optionss.remove(selectedOptionIndex);
+}
+
+let buttonfnctn = document.getElementById("button");
+
+buttonfnctn.addEventListener("click",btn);
